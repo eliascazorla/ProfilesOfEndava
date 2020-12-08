73a9db7486640314b6c4f949bf4e9a5d98f3c915
ecazorla <elias.cazorla@endava.com>
2020-12-08 15:14:50 -0300

    Feature getProfiles added.

--- Backend/src/main/java/com/endava/juniorprogram/profilesofendava/repositories/ProfilesRepository.java
+++ Backend/src/main/java/com/endava/juniorprogram/profilesofendava/repositories/ProfilesRepository.java
@@ -4,6 +4,12 @@ import com.endava.juniorprogram.profilesofendava.model.Profile;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProfilesRepository extends CrudRepository<Profile, Long> {
+    List<Profile> getByFullName(String fullName);
+    List<Profile> getByTenure(long tenure);
+    List<Profile> getBySeniority(String seniority);
+    List<Profile> findBySkills_Skill_Name(String name);
 }
--- Backend/src/main/java/com/endava/juniorprogram/profilesofendava/rest/ProfileController.java
+++ Backend/src/main/java/com/endava/juniorprogram/profilesofendava/rest/ProfileController.java
@@ -1,13 +1,38 @@
 package com.endava.juniorprogram.profilesofendava.rest;
 
+import com.endava.juniorprogram.profilesofendava.model.Profile;
 import com.endava.juniorprogram.profilesofendava.services.IProfilesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/profiles")
 public class ProfileController {
     @Autowired
     private IProfilesService profilesService;
+
+    @GetMapping
+    public List<Profile> getProfiles(@RequestParam(required = false) String fullName,
+                                     @RequestParam(required = false)Long tenure,
+                                     @RequestParam(required = false)String seniority,
+                                     @RequestParam(required = false)String skill){
+        if(fullName!=null){
+            return profilesService.getByFullName(fullName);
+        }
+        if(tenure!=null){
+            return profilesService.getByTenure((long) tenure);
+        }
+        if(seniority!=null) {
+            return profilesService.getBySeniority(seniority);
+        }
+        if(skill!=null){
+            return profilesService.getBySkill(skill);
+        }
+        return profilesService.getAll();
+    }
 }
