8703ab0d08372cc01af5d38af9300ff774bdac42
ecazorla <elias.cazorla@endava.com>
2020-12-11 12:59:15 -0300

    Added the functionality getProfiles using hooks in ViewProfiles

--- Frontend/profiles-of-endava/src/components/CardsContainerComponent/CardsContainer.tsx
+++ Frontend/profiles-of-endava/src/components/CardsContainerComponent/CardsContainer.tsx
@@ -5,13 +5,9 @@ import Card from '../CardComponent/Card';
 
 interface CardsContainerProps {
     profiles:Profile[];
-    test?:any;
 }
 
 function CardsContainer (props: CardsContainerProps) {
-  /*setTimeout(() => {
-    props.test("test" + new Date());
-  }, 1000)*/
   return (
     <div className="cards_container">
         {props.profiles.map(profile => <Card profile={profile}/>)}
